188e5ed2b48d18872ae61ab1160c8cda4b322fcf
zhiwei.liu <270274978@qq.com>
2018-01-19 10:53:20 +0800

    增加一些test code

---
+++ CrossRoad/Assets/test/test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+    public Transform to;
+    public float speed = 0.01f;
+    void Update() {
+        float step = speed * Time.deltaTime;
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, to.rotation,-Time.time * speed);
+    }
+}
