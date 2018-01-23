913dd92c0e9cd4cf196e70c7337732d43e6e495d
zhiwei.liu <270274978@qq.com>
2018-01-23 11:55:37 +0800

    增加闪烁效果

--- CrossRoad/Assets/Scripts/ui/WarningUICtr.cs
+++ CrossRoad/Assets/Scripts/ui/WarningUICtr.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class WarningUICtr : MonoBehaviour {
 	public Image m_img ; 
@@ -14,10 +15,11 @@ public class WarningUICtr : MonoBehaviour {
 		m_img.transform.position = new Vector3(posX,0,0);
 
 		StartCoroutine(waitForDestory());
+		this.doAction();
 	}
 
 	IEnumerator waitForDestory() {
-		yield return new WaitForSeconds(0.3f); 
+		yield return new WaitForSeconds(0.4f); 
 
 		this.OnDestroy();
 	}
@@ -26,5 +28,10 @@ public class WarningUICtr : MonoBehaviour {
 	{
 		gameObject.SetActive(false);
 		Destroy(gameObject);
+	}
+
+	void doAction() {
+		m_img.color = new Color(m_img.color.r,m_img.color.g, m_img.color.b , 0);
+		DOTween.ToAlpha(()=> m_img.color , x=> m_img.color = x , 0.8f , 0.1f ).SetLoops(4, LoopType.Yoyo);
 	}
 }
