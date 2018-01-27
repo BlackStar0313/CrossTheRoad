4e2dbc3135a7455c0a175afff5f471afc03f1f90
zhiwei.liu <270274978@qq.com>
2018-01-27 18:45:07 +0800

    增加商店ui控制

---
+++ CrossRoad/Assets/Scripts/ui/ShopUICtr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ShopUICtr : MonoBehaviour {
+	public Button m_btnHome ; 
+	public Text m_textScore;
+
+	void Start()
+	{
+		m_btnHome.onClick.AddListener(()=> handleTouch(m_btnHome));
+		m_textScore.text = PlayerManager.getInstance().GetPlayerInfo().score.ToString();
+	}
+
+	void OnDestroy()
+	{
+		m_btnHome.onClick.RemoveAllListeners();
+	}
+
+	private void handleTouch(Button btn) {
+		if (btn == m_btnHome) { 
+			SceneManager.LoadScene("Menu");
+		}
+	}
+}
