3a7faacd41e7b8f309135853f52773c70c96e1b8
zhiwei.liu <270274978@qq.com>
2018-02-07 12:28:24 +0800

    切换角色代码优化

---
+++ CrossRoad/Assets/Scripts/role/BasicShowRole.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicShowRole : MonoBehaviour {
+	private string m_currentBodyName = "Body_33_punk_D";
+	private string m_currentHeadName = "head_64_punk_C";
+	private string m_prefix = "Bip001/Bip001 Pelvis/Bip001 Spine/Bip001 Neck/Bip001 Head/HEAD_CONTAINER/";
+	
+
+	protected void ShowRole(int roleIdx) {
+		GameObject bodyObj = gameObject.transform.Find(m_currentBodyName).gameObject;
+		GameObject headObj = gameObject.transform.Find(m_prefix + m_currentHeadName).gameObject;
+
+
+		StrDatarole roleData = DataManager.getInstance().GetRoleDataByIdx(roleIdx);
+		try
+		{
+			//TODO: 先这么处理下
+			if (m_currentBodyName != roleData.body_name) {
+				bodyObj.SetActive(false);
+			}
+			if (m_currentHeadName != roleData.head_name) {
+				headObj.SetActive(false);
+			}
+
+			GameObject bodyObj1 = gameObject.transform.Find(roleData.body_name).gameObject;
+			GameObject headObj1 = gameObject.transform.Find(m_prefix + roleData.head_name).gameObject;
+			bodyObj1.SetActive(true);
+			headObj1.SetActive(true);
+			
+
+			if (roleData.glass_name != "") {
+				Transform glassTrans = gameObject.transform.Find(m_prefix + roleData.glass_name);
+				if (glassTrans != null) {
+					glassTrans.gameObject.SetActive(true);
+				}
+			}
+		}
+		catch (System.Exception)
+		{
+			Debug.LogError("wrong data ");
+			throw;
+		}
+	}
+}
